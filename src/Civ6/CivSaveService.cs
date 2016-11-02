3d8ba32bcd7b4b9b13da353e36fa32f5cd0c3d1c
Mel Green <mastermel@gmail.com>
2016-11-01 19:59:22 -0600

    Created placeholder class for Civ6 save service

---
+++ src/Civ6/CivSaveService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using MRobot.Civilization.Base;
+using System.IO;
+
+namespace MRobot.Civilization.Civ6
+{
+    public class CivSaveService : ICivSaveService
+    {
+        public IEnumerable<ICivilization> AllCivilizations
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerable<PlayerDifficulty> AllDifficulties
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerable<Expansion> AllExpansions
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerable<Map> AllMaps
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IDictionary<string, string> DefaultGameWorldOptions
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public PlayerDifficulty DefaultPlayerDifficulty
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ICivilization UknownCiv
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ICivilization FindCivById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GameConfig LoadGameConfig(Stream saveStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GameConfig CreateNewGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
